634aa4c8b91fed58b2b5c080b9b5c9a541b368cf
Emmanuel  Carbajal <CarbajalEmm07@users.noreply.github.com>
2023-05-09 20:11:53 -0600

    se sube programa

--- src/main/java/com/mycompany/factorial/Factorial.java
+++ src/main/java/com/mycompany/factorial/Factorial.java
@@ -4,13 +4,27 @@
 
 package com.mycompany.factorial;
 
+import java.util.Scanner;
+
 /**
  *
  * @author emmanuel
  */
 public class Factorial {
+   public void fact() {
+        Scanner stdIn = new Scanner(System.in);
+        int numero;
+        double factorial = 1.0;
+        System.out.print("Introduzca un número entero: ");
+        numero = stdIn.nextInt();
+        for (int i = 2; i <= numero; i++) {
+            factorial *= i;
+        }
+        System.out.println(numero + "! = " + factorial);
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Factorial Factil = new Factorial();
+        Factil.fact();
     }
 }
